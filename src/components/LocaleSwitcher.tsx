a73b6a3020a3213a778d4dc6676b530b697d85eb
Aymeric PINEAU <ay.pineau@gmail.com>
2025-03-24 15:33:53 +0100

    refactor: simplify LocaleSwitcher component and improve layout

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,9 +1,4 @@
-import {
-  Locales,
-  getHTMLTextDir,
-  getLocaleName,
-  getLocalizedUrl,
-} from "intlayer";
+import { getLocaleName, getLocalizedUrl } from "intlayer";
 import { useLocale } from "react-intlayer";
 import { type FC } from "react";
 
@@ -11,39 +6,37 @@ export const LocaleSwitcher: FC = () => {
   const { locale, availableLocales, setLocale } = useLocale({});
 
   return (
-    <div>
-      <button>{getLocaleName(locale)}</button>
-      <div id="localePopover">
-        {availableLocales.map((localeItem) => (
-          <a
-            href={getLocalizedUrl(location.pathname, localeItem)}
-            hrefLang={localeItem}
-            aria-current={locale === localeItem ? "page" : undefined}
-            onClick={(e) => {
-              e.preventDefault();
-              setLocale(localeItem);
-            }}
-            key={localeItem}
-          >
-            <span>
-              {/* Language in its own Locale - e.g. Français */}
-              {getLocaleName(localeItem, locale)}
-            </span>
-            <span dir={getHTMLTextDir(localeItem)} lang={localeItem}>
-              {/* Language in current Locale - e.g. Francés with current locale set to Locales.SPANISH */}
-              {getLocaleName(localeItem)}
-            </span>
-            <span dir="ltr" lang={Locales.ENGLISH}>
-              {/* Language in English - e.g. French */}
-              {getLocaleName(localeItem, Locales.ENGLISH)}
-            </span>
-            <span>
-              {/* Language in its own Locale - e.g. FR */}
-              {localeItem}
-            </span>
-          </a>
-        ))}
-      </div>
+    <div
+      id="localePopover"
+      style={{
+        position: "fixed",
+        bottom: "1rem",
+        display: "flex",
+        gap: "1rem",
+        right: "1rem",
+        marginTop: "4rem",
+      }}
+    >
+      {availableLocales.map((localeItem) => (
+        <a
+          href={getLocalizedUrl(location.pathname, localeItem)}
+          hrefLang={localeItem}
+          aria-current={locale === localeItem ? "page" : undefined}
+          onClick={(e) => {
+            e.preventDefault();
+            setLocale(localeItem);
+          }}
+          key={localeItem}
+          style={{
+            display: "flex",
+          }}
+        >
+          <span>
+            {/* Language in its own Locale - e.g. Français */}
+            {getLocaleName(localeItem, locale)}
+          </span>
+        </a>
+      ))}
     </div>
   );
 };
