c761e7863cbaf9dd7b8f7935af08bf1644d3d325
Aymeric PINEAU <ay.pineau@gmail.com>
2025-03-24 15:20:22 +0100

    refactor: remove editor configuration from intlayer.config.ts

--- intlayer.config.ts
+++ intlayer.config.ts
@@ -12,14 +12,6 @@ const config: IntlayerConfig = {
     ],
     defaultLocale: Locales.ENGLISH,
   },
-  editor: {
-    applicationURL: "http://localhost:5173",
-    cmsURL: "http://localhost:3000",
-    enabled: true,
-    clientId: "ec98ac93750bf8a3d45eb6012dfbeb23",
-    clientSecret:
-      "30cf6b8dcbd0ee7c3c0ab9b95ee43542d8b33fa3eba8e02ee0dd08255fc091da",
-  },
 };
 
 export default config;
