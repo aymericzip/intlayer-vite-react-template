ea4618715a074a9ca0857b1793bd3e61df98068c
Aymeric PINEAU <ay.pineau@gmail.com>
2025-05-21 13:28:34 +0200

    core: update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { intlayerPlugin } from "vite-intlayer";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), intlayerPlugin()],
+  plugins: [
+    react(),
+    intlayerPlugin(),
+    visualizer({
+      emitFile: true,
+      filename: "stats.html",
+    }),
+  ],
 });
